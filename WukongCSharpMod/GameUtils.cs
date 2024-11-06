b3e218eaf00c4055e1fc6d39c9ac7fd32330d804
Julek Kopczewski <julek.kopczewski@gmail.com>
2024-11-06 14:00:28 +0100

    Initial version of the C# mod interface

---
+++ WukongCSharpMod/GameUtils.cs
@@ -0,0 +1,52 @@
+﻿using b1;
+using UnrealEngine.Engine;
+using UnrealEngine.Runtime;
+
+namespace WukongCSharpMod
+{
+	public static class GameUtils
+	{
+		private static UWorld _world;
+		private static readonly object _lockObj = new object();
+		private static bool _isExecuting = false;
+
+		public static string Name => typeof(GameUtils).Namespace;
+
+		public static UWorld GetWorld()
+		{
+			if ((UObject)(object)_world == (UObject)null)
+			{
+				var obj = GCHelper.FindRef(FGlobals.GWorld)?.Managed;
+				_world = (UWorld)(object)((obj is UWorld) ? obj : null);
+			}
+			return _world;
+		}
+
+		public static APawn GetControlledPawn()
+		{
+			return ((AController)UGSE_EngineFuncLib.GetFirstLocalPlayerController((UObject)(object)GetWorld())).GetControlledPawn();
+		}
+
+		public static BGUPlayerCharacterCS GetBGUPlayerCharacterCS()
+		{
+			var controlledPawn = GetControlledPawn();
+			return (BGUPlayerCharacterCS)(object)((controlledPawn is BGUPlayerCharacterCS) ? controlledPawn : null);
+		}
+
+		public static BGP_PlayerControllerB1 GetPlayerController()
+		{
+			return (BGP_PlayerControllerB1)UGSE_EngineFuncLib.GetFirstLocalPlayerController((UObject)(object)GetWorld());
+		}
+
+		public static BUS_GSEventCollection GetBUS_GSEventCollection()
+		{
+			return BUS_EventCollectionCS.Get((AActor)(object)GetControlledPawn());
+		}
+
+		public static BGUPlayerCharacterCS GetThis()
+		{
+			var controlledPawn = GetControlledPawn();
+			return (BGUPlayerCharacterCS)(object)((controlledPawn is BGUPlayerCharacterCS) ? controlledPawn : null);
+		}
+	}
+}
---
+++ WukongCSharpMod/MyMod.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using CSharpModBase;
+using CSharpModBase.Input;
+
+namespace WukongCSharpMod
+{
+    public class MyMod : ICSharpMod
+    {
+        public string Name => "ModExample";
+        public string Version => "0.0.1";
+
+        public void Init()
+        {
+            Console.WriteLine("Init");
+        
+            Utils.RegisterKeyBind(ModifierKeys.Alt, Key.X, () =>
+            {
+                Console.WriteLine("Alt + X");
+
+                var playerCharacter = GameUtils.GetBGUPlayerCharacterCS();
+                if (playerCharacter != null)
+                {
+                    var pawn = playerCharacter.GetController().GetControlledPawn();
+                    var t = pawn.GetActorTransform();
+                    var loc = t.GetLocation();
+                    loc.Z += 1000f;
+                    t.SetLocation(loc);
+                    if (!pawn.SetActorTransform(t, true, out _, true))
+                    {
+                        Debug.WriteLine("Failed to teleport player.");
+                    }
+                }
+            });
+        }
+
+        public void DeInit()
+        {
+            Console.WriteLine("DeInit");
+        }
+    }    
+}
