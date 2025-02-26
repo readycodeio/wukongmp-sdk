07b46489159ea1a5581f19cb69bef3ec2351305c
Kamil Opalach <kamil.opalach@readycode.io>
2025-02-26 13:11:36 +0100

    Disable skills

---
+++ WukongApi/Patches/SkillPatches.cs
@@ -0,0 +1,31 @@
+﻿using b1;
+using HarmonyLib;
+using System.Reflection;
+
+namespace WukongApi.Patches
+{
+    [HarmonyPatch(typeof(BUS_PlayerInputActionComp), "TriggerMagicSkill")]
+    [HarmonyPatchCategory(Constants.ConnectedPatches)]
+    public static class PatchTriggerMagicSkill
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch]
+    [HarmonyPatchCategory(Constants.ConnectedPatches)]
+    public static class PatchFaBaoSkill
+    {
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.Method("b1.BUIACastFaBaoSkill:OnTriggerInputAction");
+        }
+
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
