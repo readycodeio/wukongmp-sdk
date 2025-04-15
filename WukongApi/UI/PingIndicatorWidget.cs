13bc9a3a332619b93d1d87edab3d58ccb4eb826d
Kamil Opalach <kamil.opalach@readycode.io>
2025-04-15 12:51:01 +0200

    Add ping indicator

---
+++ WukongApi/UI/PingIndicatorWidget.cs
@@ -0,0 +1,12 @@
+﻿namespace WukongApi.UI
+{
+    public class PingIndicatorWidget() : GameWidgetBase(Constants.PingWidgetName)
+    {
+        public void SetPingText(int pingInMiliseconds)
+        {
+            GameWidget?.CallFunctionByNameWithArguments($"SetPingText {pingInMiliseconds}", true);
+        }
+
+        protected override void PostInitialize() { }
+    }
+}
