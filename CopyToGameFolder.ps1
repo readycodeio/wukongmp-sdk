ceec43422459ec76e9abc23570cc9ccd7434610e
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-05-16 12:02:36 +0200

    Update copy script

--- CopyToGameFolder.ps1
+++ CopyToGameFolder.ps1
@@ -1,14 +1,16 @@
 param (
+    [string]$ModVariant,
     [string]$Configuration
 )
 
-# Debug by default
-if (-not $Configuration) {
-    $Configuration = "Debug"
+# Require parameters
+if (-not $ModVariant -or -not $Configuration) {
+    Write-Host "Usage: .\CopyToGameFolder.ps1 <variant> <configuration>"
+    Exit 1
 }
 
 # Define the source and destination directories
-$sourceDir = "WukongMpMod/bin/$Configuration/netstandard2.1"
+$sourceDir = "WukongMp.$ModVariant/bin/$Configuration/netstandard2.1"
 $steamDir = Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Valve\Steam" -Name "InstallPath" | Select-Object -ExpandProperty InstallPath
 $destDir = "$steamDir\steamapps\common\BlackMythWukong\b1\Binaries\Win64\CSharpLoader\Mods\WukongMpMod"
 
@@ -18,7 +20,7 @@ if (!(Test-Path -Path $destDir)) {
 }
 
 # Define the files to copy
-$files = @("WukongApi.dll", "WukongApi.pdb", "WukongMpMod.dll", "WukongMpMod.pdb", "ReadyM.Relay.Client.dll", "ReadyM.Relay.Client.pdb", "ReadyM.Relay.Common.dll", "ReadyM.Relay.Common.pdb", "ReadyM.Relay.Common.Wukong.dll", "ReadyM.Relay.Common.Wukong.pdb")
+$files = @("WukongMp.Api.dll", "WukongMp.Api.pdb", "WukongMpMod.dll", "WukongMpMod.pdb", "ReadyM.Relay.Client.dll", "ReadyM.Relay.Client.pdb", "ReadyM.Relay.Common.dll", "ReadyM.Relay.Common.pdb", "ReadyM.Relay.Common.Wukong.dll", "ReadyM.Relay.Common.Wukong.pdb")
 
 # Copy each file to the destination directory
 foreach ($file in $files) {
