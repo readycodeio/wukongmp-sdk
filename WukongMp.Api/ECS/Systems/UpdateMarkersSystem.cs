cee1df21dc10ec7eda4f54d28ec7e71025a98753
Kamil Opalach <kamil.opalach@readycode.io>
2025-06-06 15:05:07 +0200

    Change marker color

--- WukongMp.Api/ECS/Systems/UpdateMarkersSystem.cs
+++ WukongMp.Api/ECS/Systems/UpdateMarkersSystem.cs
@@ -5,11 +5,11 @@ using WukongMp.Api.Old;
 
 namespace WukongMp.Api.ECS.Systems;
 
-public sealed class UpdateMarkersSystem : QuerySystem<LocalTamerComponent, MarkerComponent, TranslationComponent>
+public sealed class UpdateMarkersSystem : QuerySystem<LocalTamerComponent, MarkerComponent, TranslationComponent, NicknameComponent>
 {
     protected override void OnUpdate()
     {
-        Query.ForEachEntity((ref tamer, ref marker, ref trans, _) =>
+        Query.ForEachEntity((ref tamer, ref marker, ref trans, ref name, _) =>
         {
             if (marker.MarkerActor == null)
                 return;
@@ -18,11 +18,13 @@ public sealed class UpdateMarkersSystem : QuerySystem<LocalTamerComponent, Marke
             if (tamer.Pawn != null)
             {
                 markerHeight = tamer.Pawn.CapsuleComponent.GetScaledCapsuleHalfHeight() * 1.1f;
+                marker.MarkerActor.CallFunctionByNameWithArguments($"SetText {name.Nickname} Blue", true);
                 marker.MarkerActor.SetActorLocation(trans.Position.ToFVector() + new FVector(0, 0, markerHeight), false, out var _, true);
             }
             else if (tamer.Tamer != null)
             {
                 markerHeight = tamer.Tamer.CapsuleComponent.GetScaledCapsuleHalfHeight() * 1.1f;
+                marker.MarkerActor.CallFunctionByNameWithArguments($"SetText {name.Nickname} Red", true);
                 marker.MarkerActor.SetActorLocation(trans.Position.ToFVector() + new FVector(0, 0, markerHeight), false, out var _, true);
             }
         });
