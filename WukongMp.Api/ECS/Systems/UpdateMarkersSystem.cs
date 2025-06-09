df843853b6375ac06292731f5ed4ef3922cbfd6e
Kamil Opalach <kamil.opalach@readycode.io>
2025-06-09 15:38:35 +0200

    Display current status

--- WukongMp.Api/ECS/Systems/UpdateMarkersSystem.cs
+++ WukongMp.Api/ECS/Systems/UpdateMarkersSystem.cs
@@ -5,26 +5,28 @@ using WukongMp.Api.Old;
 
 namespace WukongMp.Api.ECS.Systems;
 
-public sealed class UpdateMarkersSystem : QuerySystem<LocalTamerComponent, MarkerComponent, TranslationComponent, NicknameComponent>
+public sealed class UpdateMarkersSystem : QuerySystem<LocalTamerComponent, MarkerComponent, TranslationComponent, NicknameComponent, TamerComponent>
 {
     protected override void OnUpdate()
     {
-        Query.ForEachEntity((ref tamer, ref marker, ref trans, ref name, _) =>
+        Query.ForEachEntity((ref tamer, ref marker, ref trans, ref name, ref tam, _) =>
         {
             if (marker.MarkerActor == null)
                 return;
+
+            string title = $"ShouldBeSpawned:{tam.ShouldBeSpawned}|IsLocallySpawned:{tamer.IsLocallySpawned}|IsMonsterSynced:{tamer.IsMonsterSynced}";
 
             var markerHeight = 0f;
             if (tamer.Pawn != null)
             {
                 markerHeight = tamer.Pawn.CapsuleComponent.GetScaledCapsuleHalfHeight() * 1.1f;
-                marker.MarkerActor.CallFunctionByNameWithArguments($"SetText {name.Nickname} Blue", true);
+                marker.MarkerActor.CallFunctionByNameWithArguments($"SetText {title} Blue", true);
                 marker.MarkerActor.SetActorLocation(trans.Position.ToFVector() + new FVector(0, 0, markerHeight), false, out var _, true);
             }
             else if (tamer.Tamer != null)
             {
                 markerHeight = tamer.Tamer.CapsuleComponent.GetScaledCapsuleHalfHeight() * 1.1f;
-                marker.MarkerActor.CallFunctionByNameWithArguments($"SetText {name.Nickname} Red", true);
+                marker.MarkerActor.CallFunctionByNameWithArguments($"SetText {title} Red", true);
                 marker.MarkerActor.SetActorLocation(trans.Position.ToFVector() + new FVector(0, 0, markerHeight), false, out var _, true);
             }
         });
