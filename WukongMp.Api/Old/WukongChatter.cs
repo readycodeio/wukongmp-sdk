1a8444d10268d35a67722ac389eeb1f66a3ed6b1
Kamil Opalach <kamil.opalach@readycode.io>
2025-06-12 13:05:59 +0200

    Add rebirth in rebirth point

--- WukongMp.Api/Old/WukongChatter.cs
+++ WukongMp.Api/Old/WukongChatter.cs
@@ -49,6 +49,7 @@ namespace WukongMp.Api.Old
             _commands.Add("/reconnect", new Command(RequestReconnect));
             _commands.Add("/disconnect", new Command(RequestDisconnect));
             _commands.Add("/rebirth", new Command(RequestRebirth));
+            _commands.Add("/rebirth_point", new Command(RequestPointRebirth));
             _commands.Add("/giveup", new Command(RequestGiveUp));
             _commands.Add("/master", new Command(RequestNewMasterClient));
             _commands.Add("/spectator", new Command(SetSpectatorStatus));
@@ -88,6 +89,14 @@ namespace WukongMp.Api.Old
 
         private void RequestRebirth(ReadOnlyMemory<string> _)
         {
+            WukongMpMod.Instance.SendRebirthPlayer(WukongMpMod.Instance.RelayClient.PeerId);
+            SendServerMessage("PlayerRequestedRebirth", NickName);
+        }
+
+        private void RequestPointRebirth(ReadOnlyMemory<string> _)
+        {
+            
+            PlayerUtils.TeleportLocalPlayerToRebirthPoint();
             WukongMpMod.Instance.SendRebirthPlayer(WukongMpMod.Instance.RelayClient.PeerId);
             SendServerMessage("PlayerRequestedRebirth", NickName);
         }
