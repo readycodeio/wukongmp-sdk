9516e790aa29ff0c0f4f75ee73ddac855757369c
Kamil Opalach <kamil.opalach@readycode.io>
2025-07-04 12:55:21 +0200

    Only master client can add monsters to ECS

--- WukongMp.Api/Patches/MonsterPatches.cs
+++ WukongMp.Api/Patches/MonsterPatches.cs
@@ -95,17 +95,20 @@ namespace WukongMp.Api.Patches
             if (!WukongMP.Instance.ShouldRunConnectedPatches())
                 return;
 
-            if (__instance.TamerType != ETamerType.Summoned)
+            if (WukongMpModBase.Client.IsMasterClient)
             {
-                var guid = BGU_DataUtil.GetActorGuid(__instance);
-                var entity = WukongMpMod.Instance.GetMonsterByGuid(guid);
-                if (entity == null)
+                if (__instance.TamerType != ETamerType.Summoned)
                 {
-                    SpawningUtils.CreateMonsterInEcs(guid, __instance, 2, __instance.PathName);
-                }
-                else
-                {
-                    Logging.LogDebug("Monster already exists in ECS: {Entity}", entity.ToString());
+                    var guid = BGU_DataUtil.GetActorGuid(__instance);
+                    var entity = WukongMpMod.Instance.GetMonsterByGuid(guid);
+                    if (entity == null)
+                    {
+                        SpawningUtils.CreateMonsterInEcs(guid, __instance, 2, __instance.PathName);
+                    }
+                    else
+                    {
+                        Logging.LogDebug("Monster already exists in ECS: {Entity}", entity.ToString());
+                    }
                 }
             }
         }
