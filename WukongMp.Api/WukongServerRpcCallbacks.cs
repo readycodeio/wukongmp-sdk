81b04f5d52d5db90ebfd85f4b738a0e81854edcb
Kamil Opalach <kamil.opalach@readycode.io>
2025-07-11 16:08:40 +0200

    Add callbacks and handlers for server rpc events

---
+++ WukongMp.Api/WukongServerRpcCallbacks.cs
@@ -0,0 +1,34 @@
+﻿using ReadyM.Api.Multiplayer;
+using ReadyM.Relay.Client;
+using ReadyM.Relay.Common;
+using ReadyM.Relay.Common.Protocol.Enums;
+using System;
+
+namespace WukongMp.Api;
+
+public partial class WukongServerRpcCallbacks : IDisposable
+{
+    protected readonly RelaySerializer Serializer;
+    protected readonly IRelayClient RelayClient;
+
+    public WukongServerRpcCallbacks(
+        RelaySerializer serializer,
+        IRelayClient relayClient)
+    {
+        Serializer = serializer;
+        RelayClient = relayClient;
+
+        InitRpc();
+    }
+
+    public void Dispose()
+    {
+        DeInitRpc();
+    }
+
+    [ServerRpcEvent(ServerRpcCode.DataMessage)]
+    internal void OnDataMessage(int value)
+    {
+        Logging.LogDebug("Received data from server RPC, value: {Value}", value);
+    }
+}
