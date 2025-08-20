235975b5109a8ffb591c0d10fd09990ddf360a12
Kamil Opalach <kamil.opalach@readycode.io>
2025-08-20 16:20:51 +0200

    Implement INetSerializable interface in BuffAddData

--- WukongMp.Api/DTO/BuffAddData.cs
+++ WukongMp.Api/DTO/BuffAddData.cs
@@ -1,11 +1,12 @@
-﻿using ReadyM.Api.Multiplayer.Generators;
+﻿using LiteNetLib.Utils;
+using ReadyM.Api.Multiplayer.Generators;
 using ReadyM.Api.Serialization;
 
 namespace WukongMp.Api.DTO;
 
 [DeriveINetSerializable]
 [DeriveJsonSerializable]
-public partial struct BuffAddData(int buffId, float duration)
+public partial struct BuffAddData(int buffId, float duration) : INetSerializable
 {
     public int BuffId = buffId;
     public float Duration = duration;
