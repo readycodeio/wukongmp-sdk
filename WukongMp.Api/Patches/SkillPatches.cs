aa01c7afa659bd5b34cc9126c53b5edb74feac10
Kamil Opalach <kamil.opalach@readycode.io>
2025-08-20 17:24:31 +0200

    Revert "Disable sweep patch"
    This reverts commit d23008d2e8855bde1457197317d140212cb77f10.

--- WukongMp.Api/Patches/SkillPatches.cs
+++ WukongMp.Api/Patches/SkillPatches.cs
@@ -1028,7 +1028,7 @@ public static class PatchPendingReset
 }
 
 [HarmonyPatch]
-[HarmonyPatchCategory(Constants.DisabledPatches)]
+[HarmonyPatchCategory(Constants.CoopPatches)]
 public static class PatchOnSweepCheckHit
 {
     private static MethodBase TargetMethod()
