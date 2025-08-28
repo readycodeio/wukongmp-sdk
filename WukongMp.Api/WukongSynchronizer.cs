59ca70a0a3bfe23492a9ca85f4692a1f02264326
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-08-28 16:41:32 +0200

    Fix DI

--- WukongMp.Api/WukongSynchronizer.cs
+++ WukongMp.Api/WukongSynchronizer.cs
@@ -70,7 +70,7 @@ public class WukongSynchronizer : ClientNetworkedStateSynchronizer
 
         _syncGroup.Add(new CreateLocalMainCharacterEntitySystem(state, playerState, eventBus, Logger));
         _syncGroup.Add(new DeleteLocalMainCharacterEntitySystem(playerState, Logger));
-        _syncGroup.Add(new SpawnOtherMainCharactersSystem(state, playerState, playerPawnState, eventBus, Logger));
+        _syncGroup.Add(new SpawnOtherMainCharactersSystem(state, playerState, playerPawnState, eventBus, clientOwnership, Logger));
         _syncGroup.Add(new DespawnOtherMainCharactersSystem(archetypeEvent, playerState, wukongArchetype, playerArchetype, playerPawnState, ecsLoop, widgetManager, world, eventBus, Logger));
         _syncGroup.Add(new SyncMainCharactersSystem(playerState, modeManager, eventBus, Logger));
         _syncGroup.Add(new RespawnMainCharacterSystem(playerState, rpc, Logger));
