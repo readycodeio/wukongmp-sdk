cdac1d51c363c9e1a825c36de63f9b240fb27613
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-09-09 14:15:41 +0200

    Delete unused system

--- WukongMp.Api/WukongSynchronizer.cs
+++ WukongMp.Api/WukongSynchronizer.cs
@@ -69,7 +69,6 @@ public class WukongSynchronizer : ClientNetworkedStateSynchronizer
         _syncGroup.Add(new SyncMonsterTeamSystem());
 
         _syncGroup.Add(new CreateLocalMainCharacterEntitySystem(state, playerState, eventBus, Logger));
-        _syncGroup.Add(new DeleteLocalMainCharacterEntitySystem(playerState, Logger));
         _syncGroup.Add(new SpawnOtherMainCharactersSystem(state, playerState, playerPawnState, eventBus, clientOwnership, Logger));
         _syncGroup.Add(new DespawnOtherMainCharactersSystem(archetypeEvent, playerState, wukongArchetype, playerPawnState, widgetManager, eventBus, Logger));
         _syncGroup.Add(new SyncMainCharactersSystem(playerState, modeManager, eventBus, Logger));
