63e7a7f1a4075a577d6df2ed6350c56a809b57ef
Kamil Opalach <kamil.opalach@readycode.io>
2025-09-09 15:33:05 +0200

    Update exclude lists

--- WukongMp.Api/Configuration/DisabledCollidersData.cs
+++ WukongMp.Api/Configuration/DisabledCollidersData.cs
@@ -7,6 +7,7 @@ namespace WukongMp.Api.Configuration
         private static readonly List<string> Guids =
         [
             "1716325759611-719bd9374ae623ee09ed3983024db13e-BP_DynamicObstcle_C_1",
+            "1686949186349-6b25c5f04b1967cd5863e485d95e3cd9-BP_DynamicObstcle_C_3",
         ];
 
         public static bool IsDisabled(string guid)
