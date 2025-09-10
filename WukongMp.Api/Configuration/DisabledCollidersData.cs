144fedc24ce8780ee77a61f9d0f3038ff98bdeb6
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-09-10 14:52:05 +0200

    Disable collider for Tiger Vanguard arena

--- WukongMp.Api/Configuration/DisabledCollidersData.cs
+++ WukongMp.Api/Configuration/DisabledCollidersData.cs
@@ -6,8 +6,9 @@ namespace WukongMp.Api.Configuration
     {
         private static readonly List<string> Guids =
         [
-            "1716325759611-719bd9374ae623ee09ed3983024db13e-BP_DynamicObstcle_C_1",
-            "1686949186349-6b25c5f04b1967cd5863e485d95e3cd9-BP_DynamicObstcle_C_3",
+            "1716325759611-719bd9374ae623ee09ed3983024db13e-BP_DynamicObstcle_C_1", // act 2 Tiger's Acolyte bridge
+            "1686949186349-6b25c5f04b1967cd5863e485d95e3cd9-BP_DynamicObstcle_C_3", // Black Loong arena entrance
+            "UGuid.HFM.HuStone.DOin01", // act 2 Tiger Vanguard arena entrance
         ];
 
         public static bool IsDisabled(string guid)
