2bae70f29e2e8e83e4c2ec86306d46a2496687c3
Kamil Opalach <kamil.opalach@readycode.io>
2025-09-10 17:20:29 +0200

    Add colliders to disable

--- WukongMp.Api/Configuration/DisabledCollidersData.cs
+++ WukongMp.Api/Configuration/DisabledCollidersData.cs
@@ -9,6 +9,8 @@ namespace WukongMp.Api.Configuration
             "1716325759611-719bd9374ae623ee09ed3983024db13e-BP_DynamicObstcle_C_1", // act 2 Tiger's Acolyte bridge
             "1686949186349-6b25c5f04b1967cd5863e485d95e3cd9-BP_DynamicObstcle_C_3", // Black Loong arena entrance
             "UGuid.HFM.HuStone.DOin01", // act 2 Tiger Vanguard arena entrance
+            "1700513547161-6b25c5f04b1967cd5863e485d95e3cd9-BP_DynamicObstcle_C_1", // act 2 Tiger Vanguard
+            "1654114656537-719bd9374ae623ee09ed3983024db13e-BP_DynamicObstcle_C_5" // act 2 Yellow-Robed Squire
         ];
 
         public static bool IsDisabled(string guid)
