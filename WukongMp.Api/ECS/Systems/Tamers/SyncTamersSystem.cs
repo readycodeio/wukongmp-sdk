51de8698ca7632c9c9e51ef2482daefbe19a3ca9
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-09-25 13:52:58 +0200

    Optimize SyncTamersSystem

--- WukongMp.Api/ECS/Systems/Tamers/SyncTamersSystem.cs
+++ WukongMp.Api/ECS/Systems/Tamers/SyncTamersSystem.cs
@@ -5,25 +5,25 @@ using Friflo.Engine.ECS.Systems;
 using ReadyM.Relay.Common.Wukong.ECS.Components;
 using UnrealEngine.Engine;
 using WukongMp.Api.ECS.Components;
-using WukongMp.Api.ECS.Entities;
 using WukongMp.Api.WukongUtils;
 
 namespace WukongMp.Api.ECS.Systems.Tamers;
 
 public sealed class SyncTamersSystem : QuerySystem<TamerComponent, LocalTamerComponent>
 {
+    private const ulong TickInterval = 10; // Check every 10 ticks
+    private ulong tickCounter;
+
     protected override void OnUpdate()
     {
-        var allTamers = UGameplayStatics.GetAllActorsOfClass<BUTamerActor>(GameUtils.GetWorld())
-            ?.Where(x => x != null)
-            .GroupBy(x => BGU_DataUtil.GetActorGuid(x))
-            .ToDictionary(g => g.Key, g => g.Last());
-
-        if (allTamers is null)
-        {
-            Logging.LogError("Failed to find all tamers in the world.");
+        if (tickCounter++ % TickInterval != 0)
             return;
-        }
+
+        var allTamers =
+            UGameplayStatics.GetAllActorsOfClass<BUTamerActor>(GameUtils.GetWorld())
+                .Where(x => x != null)
+                .GroupBy(x => x.GetFinalGuid())
+                .ToDictionary(g => g.Key, g => g.Last());
 
         Query.ForEachEntity((ref TamerComponent tamerComp, ref LocalTamerComponent localTamerComp, Entity entity) =>
         {
@@ -31,7 +31,7 @@ public sealed class SyncTamersSystem : QuerySystem<TamerComponent, LocalTamerCom
             {
                 if (tamerComp.Guid is null)
                 {
-                    Logging.LogWarning("Entity {EntityId} has a TamerComponent with a null Guid. Cannot sync tamer.", entity.Id);
+                    Logging.LogError("Entity {EntityId} has a TamerComponent with a null Guid. Cannot sync tamer.", entity.Id);
                     return;
                 }
 
@@ -40,9 +40,9 @@ public sealed class SyncTamersSystem : QuerySystem<TamerComponent, LocalTamerCom
                     localTamerComp.Tamer = actor;
                     localTamerComp.IsTamerSynced = true;
 
+#if TESTING
                     ref var nameComp = ref entity.GetComponent<NicknameComponent>();
                     nameComp.Nickname = actor.GetClass().GetName();
-#if TESTING
                     MarkerUtils.CreateMarkerForCharacter(new TamerEntity(entity));
 #endif
                     Logging.LogDebug("Found matching tamer with guid: {Guid}", tamerComp.Guid);
