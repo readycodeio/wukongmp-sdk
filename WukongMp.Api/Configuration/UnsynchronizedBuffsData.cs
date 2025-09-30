9b3ee0f5ce5674d2bb35e5136edda16c7dbf6dfa
Kamil Opalach <kamil.opalach@readycode.io>
2025-09-30 17:17:57 +0200

    Do not send tamer unload buff

---
+++ WukongMp.Api/Configuration/UnsynchronizedBuffsData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WukongMp.Api.Configuration
+{
+    internal static class UnsynchronizedBuffsData
+    {
+        public static readonly List<int> Ids =
+        [
+            931 // tamer unload buff
+        ];
+    }
+}
