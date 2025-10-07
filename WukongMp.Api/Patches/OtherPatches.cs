6ef7eb1e3111e0d28a5db7c5750fad00cf269f4d
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-10-07 12:33:58 +0200

    Fix random Dictionary.Add crash

---
+++ WukongMp.Api/Patches/OtherPatches.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GSDispLib;
+using HarmonyLib;
+using WukongMp.Api.Configuration;
+
+namespace WukongMp.Api.Patches;
+
+public static class OtherPatches
+{
+    [HarmonyPatch(typeof(BUS_DispLibUnitMaterialsManageComp), "Internal_AddMaterialInfoForNewPrimComp")]
+    [HarmonyPatchCategory(Constants.ConnectedPatches)]
+    public static class PatchRandomCrashOnMeshAssignedOnTamerReset
+    {
+        public static Exception? Finalizer()
+        {
+            // suppress System.ArgumentException: An item with the same key has already been added. Key: 274753
+            return null;
+        }
+    }
+}
