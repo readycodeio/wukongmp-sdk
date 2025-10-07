e54895cb4c843f908cdcee3dd53cc8a8e908b811
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-10-07 13:56:10 +0200

    Add finalizer for GetSimpleOverlapActorsByMask

--- WukongMp.Api/Patches/OverlapPatches.cs
+++ WukongMp.Api/Patches/OverlapPatches.cs
@@ -22,7 +22,7 @@ public static class PatchThreadFunc
     {
         Monitor.Enter(PatchOverlapUtils.OverlapLock);
     }
-    
+
     public static void Finalizer()
     {
         Monitor.Exit(PatchOverlapUtils.OverlapLock);
@@ -37,7 +37,7 @@ public static class PatchOnRegisterEntityUpdatenfo
     {
         Monitor.Enter(PatchOverlapUtils.OverlapLock);
     }
-    
+
     public static void Finalizer()
     {
         Monitor.Exit(PatchOverlapUtils.OverlapLock);
@@ -52,7 +52,7 @@ public static class PatchOnDeregisterEntity
     {
         Monitor.Enter(PatchOverlapUtils.OverlapLock);
     }
-    
+
     public static void Finalizer()
     {
         Monitor.Exit(PatchOverlapUtils.OverlapLock);
@@ -63,23 +63,26 @@ public static class PatchOnDeregisterEntity
 [HarmonyPatchCategory(Constants.GlobalPatches)]
 public static class PatchGetOverlapGridIndexList
 {
-    [ThreadStatic] private static List<int>? _list;
-    [ThreadStatic] private static Func<BGC_SimpleOverlapMgrData, float>? _getter;
-    
+    [ThreadStatic]
+    private static List<int>? _list;
+
+    [ThreadStatic]
+    private static Func<BGC_SimpleOverlapMgrData, float>? _getter;
+
     private static bool IsRectangleOverlap(
         FVector2D StartPoint,
         FVector2D EndPoint,
         FVector2D OverlapStartPoint,
         FVector2D OverlapEndPoint)
     {
-        return (double) StartPoint.X != (double) EndPoint.X && 
-               (double) StartPoint.Y != (double) EndPoint.Y &&
-               (double) OverlapStartPoint.X != (double) OverlapEndPoint.X && 
-               (double) OverlapStartPoint.Y != (double) OverlapEndPoint.Y && 
-               (double) EndPoint.X > (double) OverlapStartPoint.X && 
-               (double) StartPoint.X < (double) OverlapEndPoint.X &&
-               (double) EndPoint.Y > (double) OverlapStartPoint.Y && 
-               (double) StartPoint.Y < (double) OverlapEndPoint.Y;
+        return StartPoint.X != (double)EndPoint.X &&
+               StartPoint.Y != (double)EndPoint.Y &&
+               OverlapStartPoint.X != (double)OverlapEndPoint.X &&
+               OverlapStartPoint.Y != (double)OverlapEndPoint.Y &&
+               EndPoint.X > (double)OverlapStartPoint.X &&
+               StartPoint.X < (double)OverlapEndPoint.X &&
+               EndPoint.Y > (double)OverlapStartPoint.Y &&
+               StartPoint.Y < (double)OverlapEndPoint.Y;
     }
 
     public static bool Prefix(
@@ -99,36 +102,46 @@ public static class PatchGetOverlapGridIndexList
         }
 
         var gridSize = _getter.Invoke(__instance);
-        var StartPoint = GridInfo.CenterLocation - new FVector2D(4.5 * (double)gridSize, 4.5 * (double)gridSize);
-        var EndPoint = GridInfo.CenterLocation + new FVector2D(4.5 * (double)gridSize, 4.5 * (double)gridSize);
+        var StartPoint = GridInfo.CenterLocation - new FVector2D(4.5 * gridSize, 4.5 * gridSize);
+        var EndPoint = GridInfo.CenterLocation + new FVector2D(4.5 * gridSize, 4.5 * gridSize);
         var OverlapStartPoint = Location - SquareSize;
         var OverlapEndPoint = Location + SquareSize;
-        var num1 = IsRectangleOverlap(StartPoint, EndPoint, OverlapStartPoint, OverlapEndPoint) ? 1 : 0; 
-        OverlapStartPoint = new FVector2D((double)FMath.Max(OverlapStartPoint.X, StartPoint.X), (double)FMath.Max(OverlapStartPoint.Y, StartPoint.Y));
-        OverlapEndPoint = new FVector2D((double)FMath.Min(OverlapEndPoint.X, EndPoint.X), (double)FMath.Min(OverlapEndPoint.Y, EndPoint.Y));
+        var num1 = IsRectangleOverlap(StartPoint, EndPoint, OverlapStartPoint, OverlapEndPoint) ? 1 : 0;
+        OverlapStartPoint = new FVector2D(FMath.Max(OverlapStartPoint.X, StartPoint.X), FMath.Max(OverlapStartPoint.Y, StartPoint.Y));
+        OverlapEndPoint = new FVector2D(FMath.Min(OverlapEndPoint.X, EndPoint.X), FMath.Min(OverlapEndPoint.Y, EndPoint.Y));
         var num2 = (double)FMath.Max(OverlapStartPoint.X, StartPoint.X);
         var num3 = FMath.Min(OverlapEndPoint.X, EndPoint.X);
         var x = (double)GridInfo.CenterLocation.X;
         var num4 = (float)(num2 - x);
         var num5 = num4 % gridSize;
-        var num6 = 4 + (int)((double)num4 / (double)gridSize) + ((double)num4 < 0.0 ? -1 : 1) * ((double)FMath.Abs(num5) > (double)gridSize / 2.0 ? 1 : 0);
+        var num6 = 4 + (int)(num4 / (double)gridSize) + (num4 < 0.0 ? -1 : 1) * (FMath.Abs(num5) > gridSize / 2.0 ? 1 : 0);
         var num7 = num3 - GridInfo.CenterLocation.X;
         var num8 = num7 % gridSize;
-        var num9 = 4 + (int)((double)num7 / (double)gridSize) + ((double)num7 < 0.0 ? -1 : 1) * ((double)FMath.Abs(num8) > (double)gridSize / 2.0 ? 1 : 0);
+        var num9 = 4 + (int)(num7 / (double)gridSize) + (num7 < 0.0 ? -1 : 1) * (FMath.Abs(num8) > gridSize / 2.0 ? 1 : 0);
         var num10 = (double)FMath.Max(OverlapStartPoint.Y, StartPoint.Y);
         var num11 = FMath.Min(OverlapEndPoint.Y, EndPoint.Y);
         var y = (double)GridInfo.CenterLocation.Y;
         var num12 = (float)(num10 - y);
         var num13 = num12 % gridSize;
-        var num14 = 4 + (int)((double)num12 / (double)gridSize) + ((double)num12 < 0.0 ? -1 : 1) * ((double)FMath.Abs(num13) > (double)gridSize / 2.0 ? 1 : 0);
+        var num14 = 4 + (int)(num12 / (double)gridSize) + (num12 < 0.0 ? -1 : 1) * (FMath.Abs(num13) > gridSize / 2.0 ? 1 : 0);
         var num15 = num11 - GridInfo.CenterLocation.Y;
         var num16 = num15 % gridSize;
-        var num17 = 4 + (int)((double)num15 / (double)gridSize) + ((double)num15 < 0.0 ? -1 : 1) * ((double)FMath.Abs(num16) > (double)gridSize / 2.0 ? 1 : 0);
+        var num17 = 4 + (int)(num15 / (double)gridSize) + (num15 < 0.0 ? -1 : 1) * (FMath.Abs(num16) > gridSize / 2.0 ? 1 : 0);
         OutIndexList.Add(num6);
         OutIndexList.Add(num9);
         OutIndexList.Add(num14);
         OutIndexList.Add(num17);
         __result = num1 != 0;
         return false;
+    }
+}
+
+[HarmonyPatch(typeof(BGC_SimpleOverlapMgrData), "GetSimpleOverlapActorsByMask")]
+[HarmonyPatchCategory(Constants.GlobalPatches)]
+public static class PatchGetSimpleOverlapActorsByMask
+{
+    public static Exception? Finalizer()
+    {
+        return null;
     }
 }
