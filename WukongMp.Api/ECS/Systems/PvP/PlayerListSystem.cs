97ff79abd3c9ffb64c7a45081b3b540f1c65f72a
Kamil Opalach <kamil.opalach@readycode.io>
2025-10-22 15:49:19 +0200

    Set teams with current members

--- WukongMp.Api/ECS/Systems/PvP/PlayerListSystem.cs
+++ WukongMp.Api/ECS/Systems/PvP/PlayerListSystem.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using Friflo.Engine.ECS;
 using Friflo.Engine.ECS.Systems;
 using ReadyM.Relay.Common.Wukong.ECS.Components;
+using WukongMp.Api.Configuration;
 using WukongMp.Api.State;
 using WukongMp.Api.UI;
 
@@ -19,16 +21,35 @@ public sealed class PlayerListSystem(WukongPlayerState playerState) : QuerySyste
 
         _timer.Restart();
 
+        List<string> redTeamList = [];
+        List<string> blueTeamList = [];
+        List<string> spectatorsList = [];
+
         Query.ForEachEntity((ref MainCharacterComponent mainCharacterComponent, ref PvPComponent pvp, Entity _) =>
         {
             var player = playerState.GetPlayerById(mainCharacterComponent.PlayerId);
             if (player.HasValue)
             {
                 var team = player.Value.GetState().TeamId;
-                LobbyStatusWidget.Instance.UpdatePlayerTeam(mainCharacterComponent.CharacterNickName, team, pvp.IsSpectator);
+                if (pvp.IsSpectator)
+                {
+                    spectatorsList.Add(mainCharacterComponent.CharacterNickName);
+                    return;
+                }
+                else if (team == Constants.AvailableTeamIds[0])
+                {
+                    redTeamList.Add(mainCharacterComponent.CharacterNickName);
+                    return;
+                }
+                else if (team == Constants.AvailableTeamIds[1])
+                {
+                    blueTeamList.Add(mainCharacterComponent.CharacterNickName);
+                    return;
+                }
             }
         });
 
+        LobbyStatusWidget.Instance.SetTeams(redTeamList, blueTeamList, spectatorsList);
         LobbyStatusWidget.Instance.SetConnectedCount(Query.Count);
     }
 }
