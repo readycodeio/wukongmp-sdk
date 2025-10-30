c905c95694c8c7f61b42f886f41e2183d24a4099
Kamil Opalach <kamil.opalach@readycode.io>
2025-10-30 15:15:53 +0100

    Add log to SpawnTamerSystem

--- WukongMp.Api/ECS/Systems/Tamers/SpawnTamersSystem.cs
+++ WukongMp.Api/ECS/Systems/Tamers/SpawnTamersSystem.cs
@@ -32,13 +32,21 @@ public sealed class SpawnTamersSystem(ClientState state) : QuerySystem<MetadataC
             ref LocalTamerComponent localTamerComp,
             Entity entity) =>
         {
-            if (!localTamerComp.IsTamerSynced || localTamerComp.Tamer == null)
-                return;
-
             // FIXME: Are some of those flags supposed to be removed now that all monsters are in ECS (including the
             // ones spawned in PVP?)
             if (localTamerComp.IsMonsterActive || !tamerComp.ShouldBeSpawned)
             {
+                return;
+            }
+
+            if (!localTamerComp.IsTamerSynced)
+            {
+                return;
+            }
+
+            if (localTamerComp.Tamer == null)
+            {
+                Logging.LogWarning("Tamer is null in SpawnTamersSystem, guid {Guid}", tamerComp.Guid);
                 return;
             }
 
