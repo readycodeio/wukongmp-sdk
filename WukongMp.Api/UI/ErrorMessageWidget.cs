d0f0104f7b52c6434b09bd349700c562b83cec0b
Kamil Opalach <kamil.opalach@readycode.io>
2025-10-30 13:01:43 +0100

    Add error widget

---
+++ WukongMp.Api/UI/ErrorMessageWidget.cs
@@ -0,0 +1,24 @@
+﻿using WukongMp.Api.Configuration;
+
+namespace WukongMp.Api.UI
+{
+    public class ErrorMessageWidget : GameWidgetBase
+    {
+        private static ErrorMessageWidget? _instance;
+        public static ErrorMessageWidget Instance => _instance ??= new();
+
+        private ErrorMessageWidget() : base(Constants.ErrorMessageWidgetName) { }
+
+        public void SetText(string message)
+        {
+            GameWidget?.CallFunctionByNameWithArguments($"SetText {message}", true);
+        }
+
+        public void ClearMessages()
+        {
+            SetText("");
+        }
+
+        protected override void PostInitialize() { }
+    }
+}
