9493e5be87258dee917a50ca0864e24de9a31440
Kamil Opalach <kamil.opalach@readycode.io>
2025-12-05 14:14:15 +0100

    Add logs for collision enabling

--- WukongMp.Api/Patches/DebugPatches.cs
+++ WukongMp.Api/Patches/DebugPatches.cs
@@ -1,5 +1,6 @@
 ﻿using b1;
 using HarmonyLib;
+using UnrealEngine.Engine;
 using WukongMp.Api.Configuration;
 
 namespace WukongMp.Api.Patches;
@@ -30,6 +31,30 @@ public class EnableConsoleCommandsPatch2
     {
         __result = true;
         return false;
+    }
+}
+
+[HarmonyPatch(typeof(AActor), nameof(AActor.SetActorEnableCollision))]
+[HarmonyPatchCategory(Constants.GlobalPatches)]
+public class SetActorEnableCollision
+{
+    public static void Postfix(AActor __instance, bool bNewActorEnableCollision)
+    {
+        if (DI.Instance.PawnState.GetEntityByPlayerPawn(__instance).HasValue)
+        {
+            Logging.LogDebug("SetActorEnableCollision called for player pawn actor: {0}, collision enabled: {1}", BGU_DataUtil.GetActorGuid(__instance), bNewActorEnableCollision);
+            Logging.LogDebug(new System.Diagnostics.StackTrace().ToString());
+        }
+    }
+}
+
+[HarmonyPatch(typeof(UPrimitiveComponent), nameof(UPrimitiveComponent.SetCollisionEnabled))]
+[HarmonyPatchCategory(Constants.GlobalPatches)]
+public class SetCollisionEnabled
+{
+    public static void Postfix(UPrimitiveComponent __instance, ECollisionEnabled NewType)
+    {
+        Logging.LogDebug("SetCollisionEnabled called for component: {0}, new collision type: {1}",__instance.GetName(), NewType);
     }
 }
 
