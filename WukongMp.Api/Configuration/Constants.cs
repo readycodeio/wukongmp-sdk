7caf0576e55687a7218a669ee1da2fac6b323ff7
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-12-09 14:43:52 +0100

    Force play Yellowbrow phase transition cutscene

--- WukongMp.Api/Configuration/Constants.cs
+++ WukongMp.Api/Configuration/Constants.cs
@@ -28,9 +28,10 @@ namespace WukongMp.Api.Configuration
         public const float BaseMarkerHeightCoefficient = 0.12f;
         public const float MaxMarkerHeightDistance = 10000f;
         public const float ColliderDisableTime = 3f; // seconds
-        
+
         public static readonly HashSet<int> InstantTriggerSequences =
         [
+            30105200, // act 3 boss transition to 2nd phase
             40104151, // phase 2 of Hundred-Eyed Daoist Master, sword-swallowing cutscene
             62103371, 62103351, 62103321, 62103301 // 4 heavenly kings, lute guy
         ];
