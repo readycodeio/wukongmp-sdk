7172c928d16ac5be32351087dba382d18fb8bc28
Kamil Opalach <kamil.opalach@readycode.io>
2026-01-27 18:15:52 +0100

    Add timer widget and controller

---
+++ WukongMp.Api/Helpers/TimerController.cs
@@ -0,0 +1,57 @@
+﻿using WukongMp.Api.UI;
+
+namespace WukongMp.Api.Helpers
+{
+    public class TimerController
+    {
+        private readonly WukongWidgetManager _widgetManager;
+        private CountdownTimer? _timer;
+        private int _initialMinutes;
+        private int _initialSeconds;
+
+        public TimerController(WukongWidgetManager widgetManager)
+        {
+            _widgetManager = widgetManager;
+        }
+
+        public void SetTimer(int minutes, int seconds)
+        {
+            _initialMinutes = minutes;
+            _initialSeconds = seconds;
+            _timer = new CountdownTimer(minutes, seconds);
+            _widgetManager.SetTimerVisibility(true);
+            _widgetManager.SetTimerText(_initialMinutes, _initialSeconds);
+        }
+
+        public void StartTimer()
+        {
+            _timer?.Start(
+                onFinishedCallback: OnTimerFinished,
+                onTickCallback: OnTimerTick
+            );
+        }
+
+        private void OnTimerTick(int minutes, int seconds)
+        {
+            _widgetManager.SetTimerText(minutes, seconds);
+        }
+
+        private void OnTimerFinished()
+        {
+            _widgetManager.SetTimerText(0, 0);
+            _widgetManager.SetTimerVisibility(false);
+        }
+
+        public void StopTimer()
+        {
+            _timer?.Stop();
+        }
+
+        public void ResetTimer()
+        {
+            _timer?.Reset();
+            _widgetManager.SetTimerVisibility(true);
+            _widgetManager.SetTimerText(_initialMinutes, _initialSeconds);
+        }
+    }
+}
---
+++ WukongMp.Api/UI/TimerWidget.cs
@@ -0,0 +1,18 @@
+﻿namespace WukongMp.Api.UI
+{
+    public class TimerWidget : GameWidgetBase
+    {
+        private const string TimerWidgetPath = "/Game/Mods/WukongMod/WBP_Timer.WBP_Timer_C";
+
+        public TimerWidget() : base(TimerWidgetPath)
+        {
+        }
+
+        public void SetText(int minutes, int seconds)
+        {
+            GameWidget?.CallFunctionByNameWithArguments($"SetText {minutes} {seconds}", true);
+        }
+
+        protected override void PostInitialize() { }
+    }
+}
