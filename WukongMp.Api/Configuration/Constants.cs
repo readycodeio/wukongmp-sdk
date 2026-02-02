8c24e622d2385e2d726882dc836ea3d727082ca6
Kamil Opalach <kamil.opalach@readycode.io>
2026-02-02 13:36:09 +0100

    Fix spawn monster height

--- WukongMp.Api/Configuration/Constants.cs
+++ WukongMp.Api/Configuration/Constants.cs
@@ -13,7 +13,7 @@ namespace WukongMp.Api.Configuration
         public const string GlobalPatches = "Global";
         public const string DisabledPatches = "Disabled";
         public const float MonsterSpawnDistance = 2000f;
-        public const float MonsterSpawnTraceHeight = 10000f;
+        public const float MonsterSpawnTraceHeight = 2000f;
         public const float MonsterHalfHeight = 200f;
         public const float MonsterSpawnSpread = 200f;
         public const float CameraArmLength = 720;
