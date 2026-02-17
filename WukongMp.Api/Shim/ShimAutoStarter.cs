4bfcea9023ffa1bcbc860cbbf756187874d84ab9
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-02-17 14:40:17 +0100

    Post-rebase fix (server)

--- WukongMp.Api/Shim/ShimAutoStarter.cs
+++ WukongMp.Api/Shim/ShimAutoStarter.cs
@@ -22,12 +22,12 @@ public class ShimAutoStarter : IDisposable
     private readonly ILogger _logger;
 
     private readonly ShimPlaybackRelayClient _playbackClient;
-    
+
     private readonly ShimRelayRecorder _recorder;
     private readonly IRelayClient _recorderRelayClient;
     private readonly IBlobClient _recorderRelayBlobClient;
     private readonly RelayClientService _recorderRelayService;
-    
+
     public bool ShouldAutoRecord { get; set; }
     public bool ShouldAutoPlay { get; set; }
 
@@ -49,26 +49,26 @@ public class ShimAutoStarter : IDisposable
     {
         _clientState = clientState;
         _eventBus = eventBus;
-        
+
         _ecsLoop = ecsLoop;
         _shimEcsLoop = shimEcsLoop;
-        
+
         _playbackClient = playbackClient;
-        
+
         _recorder = recorder;
         _recorderRelayClient = _recorder.AttachedRelayClient;
         _recorderRelayBlobClient = recorderRelayBlobClient;
         _recorderRelayService = recorderRelayService;
-        
+
         _logger = logger;
-        
+
         _eventBus.OnBeginLoadGameplayLevel += OnBeginLoadGameplayLevelHandler;
         _eventBus.OnEndPlayGameplayLevel += OnEndPlayGameplayLevelHandler;
 
         _ecsLoop.OnStarted += OnEcsStartedHandler;
         _ecsLoop.OnStopped += OnEcsStoppedHandler;
         _ecsLoop.OnUpdateLoop += OnEcsUpdateLoopHandler;
-        
+
         _recorder.OnRecordingStarted += OnRecordingStartedHandler;
         _recorder.OnRecordingStopped += OnRecordingStoppedHandler;
     }
@@ -76,28 +76,28 @@ public class ShimAutoStarter : IDisposable
     public void Dispose()
     {
         _recordingStartedTask?.GetAwaiter().GetResult();
-        
+
         if (_playbackClient.IsPlaying)
         {
             _playbackClient.StopPlaying();
         }
-        
+
         _recorder.OnRecordingStopped -= OnRecordingStoppedHandler;
         _recorder.OnRecordingStarted -= OnRecordingStartedHandler;
-        
+
         _ecsLoop.OnUpdateLoop -= OnEcsUpdateLoopHandler;
         _ecsLoop.OnStopped -= OnEcsStoppedHandler;
         _ecsLoop.OnStarted -= OnEcsStartedHandler;
-        
+
         _eventBus.OnEndPlayGameplayLevel -= OnEndPlayGameplayLevelHandler;
         _eventBus.OnBeginLoadGameplayLevel -= OnBeginLoadGameplayLevelHandler;
     }
-    
+
     private void OnEcsStartedHandler()
     {
         _shimEcsLoop.Start();
     }
-    
+
     private void OnEcsStoppedHandler()
     {
         _shimEcsLoop.Stop();
@@ -115,6 +115,7 @@ public class ShimAutoStarter : IDisposable
             _playbackClient.StartPlaying();
             _autoPlayingEnabled = true;
         }
+
         if (ShouldAutoRecord)
         {
             _recorder.StartRecording();
@@ -129,13 +130,14 @@ public class ShimAutoStarter : IDisposable
             _playbackClient.StopPlaying();
             _autoPlayingEnabled = false;
         }
+
         if (_autoRecordingEnabled)
         {
             _recorder.StopRecording();
             _autoRecordingEnabled = false;
         }
     }
-    
+
     private void OnRecordingStartedHandler()
     {
         _recordingStartedTask = Task.Run(OnRecordingStartedAsync);
@@ -144,10 +146,10 @@ public class ShimAutoStarter : IDisposable
     private async Task OnRecordingStartedAsync()
     {
         _logger.LogDebug("Connecting to record");
-        _recorderRelayService.Start();
+        await _recorderRelayService.StartAsync();
 
         _recorderRelayClient.RequestConnect();
-        
+
         _logger.LogDebug("Waiting for establishing connection");
 
         while (true)
@@ -155,10 +157,10 @@ public class ShimAutoStarter : IDisposable
             var connected = await _recorderRelayClient.Scheduler.RunFuncAsync(context => context.IsConnected);
             if (connected)
                 break;
-        
+
             await Task.Delay(100);
         }
-        
+
         _logger.LogDebug("Entering room");
 
         AreaId? areaId;
@@ -167,12 +169,12 @@ public class ShimAutoStarter : IDisposable
             areaId = _clientState.CurrentAreaId;
             if (areaId != null)
                 break;
-            
+
             await Task.Delay(100);
         }
-        
+
         _recorderRelayClient.RequestJoinArea(areaId.Value);
-        
+
         _logger.LogDebug("Requesting saves to record the results for shim");
         var recordSaveRelay = new WukongSaveRelay(_recorderRelayBlobClient, _logger);
         var worldSave = await recordSaveRelay.DownloadWorldSaveAsync();
@@ -181,9 +183,16 @@ public class ShimAutoStarter : IDisposable
         _logger.LogDebug("Player save downloaded: {PlayerSave}, size {Size} bytes", playerSave?.Name, playerSave?.Content.Length);
     }
 
-    private void OnRecordingStoppedHandler()
+    private async void OnRecordingStoppedHandler()
     {
-        _recorderRelayClient.RequestLeaveArea();
-        _recorderRelayService.Stop();
+        try
+        {
+            _recorderRelayClient.RequestLeaveArea();
+            await _recorderRelayService.StopAsync();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error while stopping recorder relay service");
+        }
     }
 }
