9c9bd367a124827c3150113a27dd7685439060f5
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-04-22 12:50:11 +0200

    Hotfix: fix random crash in FInputPreProcTracker (WTF)

--- WukongMp.Api/DI.cs
+++ WukongMp.Api/DI.cs
@@ -165,7 +165,9 @@ internal sealed class DI : IDependencyContainer
         Container.Register<INetworkedComponentRegistration, WukongNetworkedComponentRegistration>();
         Container.Register<INetworkedComponentRegistry, NetworkedComponentRegistry>();
 
-        Container.Register<EntityStore>();
+        // TODO | WTF? - using Register<>, which does the same thing, but lazily,
+        // TODO | causes the game to crash with a NullReferenceException in completely unrelated game code
+        Container.RegisterInstance(new EntityStore());
         Container.Register<Store>();
 
         Container.Register<ArchetypeEventRouter>();
