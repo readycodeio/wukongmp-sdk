ac290ce05ea8b59d0aa68ef8303db279d3ad9cb2
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-07-15 19:52:05 +0200

    Update build script

--- BuildModZip.ps1
+++ BuildModZip.ps1
@@ -1,12 +1,22 @@
 #!powershell.exe -ExecutionPolicy Bypass -File
 param (
+    [string] $Mode,
     [string] $Configuration
 )
 
 # Normalize params
-if (-not $Configuration)
+if ($Configuration -ne 'Debug' -and $Configuration -ne 'Release')
 {
-    Write-Host "Usage: .\BuildModZip.ps1 <Debug|Release>"
+    Write-Error "Invalid configuration specified. Use 'Debug' or 'Release'."
+    Write-Host "Usage: .\BuildModZip.ps1 <pvp|coop> <Debug|Release>"
+    Exit 1
+}
+
+
+if ($Mode -ne 'coop' -and $Mode -ne 'pvp')
+{
+    Write-Error "Invalid mode specified. Use 'coop' or 'pvp'."
+    Write-Host "Usage: .\BuildModZip.ps1 <coop|pvp> <Debug|Release>"
     Exit 1
 }
 
@@ -60,13 +70,23 @@ foreach ($p in $Mods)
 }
 
 # Append non-SDK mod files
-$allFiles += @(
-    @(@("manifest.json"), "WukongMP-co-op-mod/WukongMp.Coop/bin/$Configuration/netstandard2.0", "Mods/WukongMp.Coop"),
-    @(@("WukongMp.Coop.dll"), "WukongMP-co-op-mod/WukongMp.Coop/bin/$Configuration/netstandard2.0", "Mods/WukongMp.Coop"),
-    @(@("ArchiveSaveFile.1.sav"), "WukongMP-co-op-mod/Content", "Mods/WukongMp.Coop")
-#    @(@("WukongMp.Pvp.dll"), "WukongMp.Pvp/bin/$Configuration/netstandard2.0", "Mods/WukongMp.Pvp"),
-#    @(@("ArchiveSaveFile.0.sav"), "Deployment", "Mods/WukongMp.Pvp")
-)
+
+if ($Mode -eq 'coop')
+{
+    $allFiles += @(
+        @(@("manifest.json"), "WukongMP-co-op-mod/WukongMp.Coop/bin/$Configuration/netstandard2.0", "Mods/WukongMp.Coop"),
+        @(@("WukongMp.Coop.dll"), "WukongMP-co-op-mod/WukongMp.Coop/bin/$Configuration/netstandard2.0", "Mods/WukongMp.Coop"),
+        @(@("ArchiveSaveFile.1.sav"), "WukongMP-co-op-mod/Content", "Mods/WukongMp.Coop")
+    )
+} else {
+    $allFiles += @(
+        @(@("manifest.json"), "WukongMP-PvP-mod/WukongMp.PvP/bin/$Configuration/netstandard2.0", "Mods/WukongMp.Pvp"),
+        @(@("WukongMp.PvP.dll"), "WukongMP-PvP-mod/WukongMp.Pvp/bin/$Configuration/netstandard2.0", "Mods/WukongMp.Pvp"),
+        @(@("ArchiveSaveFile.0.sav"), "WukongMP-PvP-mod/Content", "Mods/WukongMp.Pvp"),
+        @(@("ArchiveSaveFile.1.sav"), "WukongMP-PvP-mod/Content", "Mods/WukongMp.Pvp")
+    )
+}
+
 
 # Create destination directories
 foreach ($item in $allFiles)
