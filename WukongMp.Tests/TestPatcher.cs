a16b36950ff88c6f792aa4d269cfe65b4c6be3ae
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-08-19 19:37:53 +0200

    Remove old test mod project, restructure solution file

--- WukongMp.Tests/TestPatcher.cs
+++
@@ -1,6 +0,0 @@
-using PreludeLib.Runtime.Public;
-using WukongMp.Api;
-
-namespace WukongMp.Tests;
-
-internal class TestPatcher(RuntimePrelude prelude) : WukongPatcher(typeof(Mod).Assembly, "WukongMp.Tests", prelude);
