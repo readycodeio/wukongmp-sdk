29ff28936c5326e1675ad7dcf354a210c52854d3
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-09-04 10:53:18 +0200

    Add TryGet<Component> to ReadyObject

--- WukongMp.Sdk/Entities/ReadyObject.cs
+++ WukongMp.Sdk/Entities/ReadyObject.cs
@@ -8,8 +8,7 @@ public readonly struct ReadyObject : IReadyEntity<ReadyObject>, IReadyConvertabl
     internal IWukongSynchronizationApi Api { get; }
     internal Entity Entity { get; }
 
-    // TODO: Internal
-    public ReadyObject(IWukongSynchronizationApi api, Entity entity)
+    internal ReadyObject(IWukongSynchronizationApi api, Entity entity)
     {
         Api = api;
         Entity = entity;
@@ -23,8 +22,31 @@ public readonly struct ReadyObject : IReadyEntity<ReadyObject>, IReadyConvertabl
         entity = Entity;
     }
 
+    /// <summary>
+    /// Whether the entity behind this handle no longer exists.
+    /// </summary>
+    public bool IsNull => Entity.IsNull;
+
+    /// <summary>
+    /// Reads a component, throwing if the entity is gone.
+    /// </summary>
     public ref T Get<T>() where T : struct, IComponent
     {
         return ref Entity.GetComponent<T>();
+    }
+
+    /// <summary>
+    /// Reads a copy of a component, or returns false when the entity is gone or does not carry it.
+    /// </summary>
+    public bool TryGet<T>(out T value) where T : struct, IComponent
+    {
+        if (Entity.IsNull || !Entity.HasComponent<T>())
+        {
+            value = default;
+            return false;
+        }
+
+        value = Entity.GetComponent<T>();
+        return true;
     }
 }
