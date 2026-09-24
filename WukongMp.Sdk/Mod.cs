70fee6ab4d375f35383eb5c1c6980114d4bb1592
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-09-24 16:26:30 +0200

    Rename attribute

--- WukongMp.Sdk/Mod.cs
+++ WukongMp.Sdk/Mod.cs
@@ -183,7 +183,7 @@ internal class Mod : ModBase
         // Before every other system
         DI.Instance.World.SystemRoot.Add(new ModSystemGate(DI.Instance.Resolve<IEntities>()));
 
-        // Everything a mod declared [System], which the SDK registered as this mod loaded.
+        // Everything a mod declared [Service], which the SDK registered as this mod loaded.
         DI.Instance.World.SystemRoot.Add(new ModSystemUpdates(DI.Instance));
 
         var assemblySystems = DI.Instance.Container
--- WukongMp.Sdk/ModSystemBase.cs
+++ WukongMp.Sdk/ModSystemBase.cs
@@ -5,7 +5,7 @@ using ReadyM.SDK.Client.Systems;
 namespace WukongMp.Sdk;
 
 /// Base class for plugin systems. Adds itself to the update loop on creation and removes itself on disposal.
-[Obsolete("Use [System] attribute instead.")]
+[Obsolete("Use [Service] attribute instead.")]
 public abstract class ModSystemBase
 {
     protected readonly ref struct UpdateTick(float deltaTime, float time)
